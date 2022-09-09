bacf4770b9dacdf478fb0725f1b9561cae046685
micolCODES <micolromano124@gmail.com>
2022-09-09 20:44:10 +0200

    .

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33494,7 +33494,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52276" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53406" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
