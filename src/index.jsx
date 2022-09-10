02b77bad49b2dec142f56247575cef531f4c3f77
micolCODES <micolromano124@gmail.com>
2022-09-10 17:47:53 +0200

    connecting to API

--- src/index.jsx
+++ src/index.jsx
@@ -19,3 +19,14 @@ const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
+
+app.get("/movies", function (req, res) {
+    Movies.find()
+      .then(function (movies) {
+        res.status(201).json(movies);
+      })
+      .catch(function (error) {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
+  });
